738543ae4f37aadf26d8409832906f1e3aee5925
Johann Lee <me@qinka.pro>
2019-10-12 16:22:19 +0800

    Update

--- layouts/community/ergodox/qinka/keymap.c
+++ layouts/community/ergodox/qinka/keymap.c
@@ -31,24 +31,24 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_HOME,                                        KC_END,         KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPACE,
+    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_MINUS,                                       KC_EQUAL,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPACE,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_PGUP,                                        KC_PGDOWN,      KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           RALT_T(KC_BSLASH),
     KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      RCTL_T(KC_QUOTE),
     LGUI(KC_SPACE), KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_LBRACKET,                                    KC_RBRACKET,    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_LGUI,        KC_APPLICATION, KC_LALT,                                                                                                        KC_MINUS,       KC_EQUAL,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_APPLICATION, KC_TRANSPARENT, KC_LGUI,        KC_LALT,        KC_LSHIFT,                                                                                                      TT(1),          KC_SPACE,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_CAPSLOCK,    KC_TRANSPARENT, TO(3),          TO(1),
                                                                                                                     TT(1),          LCTL(LSFT(KC_P)),
-                                                                                    KC_SPACE,       KC_LSHIFT,      KC_BSPACE,      LCTL(KC_SLASH), TT(1),          KC_ENTER
+                                                                                    LSFT_T(KC_SPACE),KC_LSHIFT,      KC_BSPACE,      LCTL(KC_SLASH), TT(1),          LT(1,KC_ENTER)
   ),
   [1] = LAYOUT_ergodox_pretty(
-    KC_GRAVE,       KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_PSCREEN,                                     KC_INSERT,      KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_DELETE,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_UP,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_SLASH,    KC_F11,
+    KC_GRAVE,       KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F11,                                         KC_F12,         KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_DELETE,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_UP,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_HOME,                                        KC_END,         KC_TRANSPARENT, KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_SLASH,    KC_F11,
     KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_4,           KC_KP_5,        KC_6,           KC_KP_ASTERISK, KC_F12,
     KC_TRANSPARENT, KC_PC_UNDO,     KC_PC_CUT,      KC_PC_COPY,     KC_PC_PASTE,    KC_TRANSPARENT, LCTL(KC_LBRACKET),                                RCTL(KC_RBRACKET),KC_TRANSPARENT, KC_KP_1,        KC_KP_2,        KC_3,           KC_KP_MINUS,    KC_EQUAL,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_KP_0,        KC_KP_DOT,      KC_TRANSPARENT, KC_KP_PLUS,     KC_RCTRL,
                                                                                                     KC_TRANSPARENT, KC_AUDIO_VOL_UP,TO(0),          TO(2),
                                                                                                                     KC_AUDIO_MUTE,  KC_CALCULATOR,
-                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_SCROLLLOCK,  TO(0),          KC_PAUSE
+                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_SCROLLLOCK,  TO(0),          KC_TRANSPARENT
   ),
   [2] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_BTN4,     KC_MS_BTN3,     KC_MS_BTN5,     KC_TRANSPARENT, KC_TRANSPARENT,
